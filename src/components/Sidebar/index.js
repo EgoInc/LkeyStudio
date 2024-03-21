134e01b3bc665a76b73f5184f26784e0d48fb9b4
Lisongcovey <62878225+LianaAlmazova@users.noreply.github.com>
2024-03-21 16:18:51 +0300

    Style changes + working language changer

--- src/components/Sidebar/index.js
+++ src/components/Sidebar/index.js
@@ -1,113 +1,123 @@
-import './sidebar.scss';
-import { Link, NavLink } from 'react-router-dom';
-import LogoS from '../../assets/images/logo-s.png';
-import LogoL from '../../assets/images/logo.svg';
-import LogoSubtitle from '../../assets/images/logo_sub.png';
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import "./sidebar.scss";
+import { useState } from "react";
+import { Link, NavLink } from "react-router-dom";
+import LogoS from "../../assets/images/logo-s.png";
+import LogoL from "../../assets/images/logo.svg";
+import LogoSubtitle from "../../assets/images/logo_sub.png";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import {
-    faTelegram,
-    faGithub,
-    faSkype,
-} from '@fortawesome/free-brands-svg-icons'
+  faTelegram,
+  faGithub,
+  faSkype,
+  faWhatsapp,
+} from "@fortawesome/free-brands-svg-icons";
 import {
-    faHome,
-    faUser,
-    faEnvelope,
-    faSuitcase,
-    faBars,
-    faClose,
-} from '@fortawesome/free-solid-svg-icons'
+  faHome,
+  faUser,
+  faEnvelope,
+  faSuitcase,
+  faBars,
+  faClose,
+} from "@fortawesome/free-solid-svg-icons";
 
-const Sidebar = () => {
-    return (
-        <>
-            <div className='nav-bar'>
-                <Link className='logo' to='/'>
-                    <img src={LogoL} alt="logo" />
-                    {/* <img className='sub-logo' src={LogoSubtitle} alt="logosub" /> */}
-                </Link>
+const Sidebar = (props) => {
+  // const [language, setLanguage] = useState(() => {
+  //   const locale = navigator.language;
+  //   return locale.startsWith("en") ? "en" : "ru"; // установка языка на основе языка браузера
+  // });
 
-                <nav>
-                    <NavLink
-                        exact="true"
-                        activeclassname="active"
-                        to="/"
-                    // onClick={() => setShowNav(false)}
-                    >
-                        <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
-                    </NavLink>
-                    <NavLink
-                        activeclassname="active"
-                        className="about-link"
-                        to="/about"
-                    // onClick={() => setShowNav(false)}
-                    >
-                        <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
-                    </NavLink>
-                    <NavLink
-                        activeclassname="active"
-                        className="portfolio-link"
-                        to="/portfolio"
-                    // onClick={() => setShowNav(false)}
-                    >
-                        <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
-                    </NavLink>
-                    <NavLink
-                        activeclassname="active"
-                        className="contact-link"
-                        to="/contact"
-                    // onClick={() => setShowNav(false)}
-                    >
-                        <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
-                    </NavLink>
-                    {/* <FontAwesomeIcon
-                    // onClick={() => setShowNav(false)}
-                    icon={faClose}
-                    color="#ffd700"
-                    size="3x"
-                    className='close-icon' /> */}
-                </nav>
-                <ul>
-                    <li>
-                        <a
-                            href="https://t.me/lisongcovey"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FontAwesomeIcon
-                                icon={faTelegram}
-                                color="#4d4d4e"
-                                className="anchor-icon"
-                            />
-                        </a>
-                    </li>
-                    <li>
-                        <a
-                            href="https://github.com/LianaAlmazova"
-                            target="_blank"
-                            rel="noreferrer"
-                        >
-                            <FontAwesomeIcon
-                                icon={faGithub}
-                                color="#4d4d4e"
-                                className="anchor-icon"
-                            />
-                        </a>
-                    </li>
-                
-                    <li>
-                        <a href="skype:live:liana.1707.la" rel="noreferrer" target="_blank">
-                            <FontAwesomeIcon
-                                icon={faSkype}
-                                color="#4d4d4e"
-                                className="anchor-icon"
-                            />
-                        </a>
-                    </li>
-                </ul>
-            </div>
-        </>
-    )
-}
+  const handleLanguageChange = (e) => {
+    props.onLanguageChanged(e.target.value);
+  };
 
-export default Sidebar
+  return (
+    <>
+      <div className="nav-bar">
+        {/* logo */}
+        <Link className="logo" to="/">
+          <img src={LogoL} alt="logo" />
+        </Link>
+
+        {/* language settings */}
+        <div className="radio-group">
+          <input
+            type="radio"
+            id="option-one"
+            name="selector"
+            value="ru-RU"
+            checked={props.language === "ru-RU"}
+            onChange={(e) => handleLanguageChange(e)}
+          />
+          <label htmlFor="option-one">Ru</label>
+
+          <input
+            type="radio"
+            id="option-three"
+            name="selector"
+            value="en-US"
+            checked={props.language === "en-US"}
+            onChange={(e) => handleLanguageChange(e)}
+          />
+          <label htmlFor="option-three">Eng</label>
+        </div>
+
+        {/* navigation */}
+        <nav>
+          <NavLink exact="true" activeclassname="active" to="/">
+            <FontAwesomeIcon icon={faHome} color="#4d4d4e" />
+          </NavLink>
+          <NavLink activeclassname="active" className="about-link" to="/about">
+            <FontAwesomeIcon icon={faUser} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            activeclassname="active"
+            className="portfolio-link"
+            to="/portfolio"
+          >
+            <FontAwesomeIcon icon={faSuitcase} color="#4d4d4e" />
+          </NavLink>
+          <NavLink
+            activeclassname="active"
+            className="contact-link"
+            to="/contact"
+          >
+            <FontAwesomeIcon icon={faEnvelope} color="#4d4d4e" />
+          </NavLink>
+        </nav>
+
+        {/* links */}
+        <ul>
+          <li>
+            <a
+              href="https://t.me/+79940199114"
+              target="_blank"
+              rel="noreferrer"
+            >
+              <FontAwesomeIcon
+                icon={faTelegram}
+                color="#4d4d4e"
+                className="anchor-icon"
+              />
+            </a>
+          </li>
+
+          <li>
+            <a
+              href="https://api.whatsapp.com/send/?phone=79940199114"
+              rel="noreferrer"
+              target="_blank"
+            >
+              <FontAwesomeIcon
+                icon={faWhatsapp}
+                color="#4d4d4e"
+                className="anchor-icon"
+              />
+            </a>
+          </li>
+        </ul>
+      </div>
+    </>
+  );
+};
+
+export default Sidebar;
